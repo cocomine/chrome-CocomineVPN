07dbe6058d6975b569bdf293d0d8b64e0f20dd5d
cocomine <roguish.6888467468@gmail.com>
2024-04-24 23:43:31 +0800

    recreate README.md

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
 chrome.runtime.onInstalled.addListener(({ reason }) => {
     if (reason === 'install') {
-        //chrome.tabs.create({ url: 'https://www.google.com' });
+        chrome.tabs.create({ url: 'https://github.com/cocomine/chrome-vpn/blob/dev/README.md' });
     }
 });
