cac147cae9db17b9efc2326187e19fe1d6191643
cocomine <roguish.6888467468@gmail.com>
2024-09-05 19:33:49 +0800

    改良檔案結構
    Signed-off-by: cocomine <roguish.6888467468@gmail.com>

---
+++ src/components/ChatGPTOnly.tsx
@@ -0,0 +1,60 @@
+import React, {useCallback} from "react";
+import useProxyData from "../hooks/useProxyData";
+import {Col, FormCheck, Row} from "react-bootstrap";
+import FormCheckInput from "react-bootstrap/FormCheckInput";
+import useProxyMode from "../hooks/useProxyMode";
+import useChatGPTOnlyData from "../hooks/useChatGPTOnlyData";
+
+/**
+ * ChatGPTOnly component
+ *
+ * This component provides a UI to toggle the "ChatGPT Only Mode".
+ * When enabled, it proxies only the chatgpt.com and openai.com websites.
+ *
+ */
+export const ChatGPTOnly: React.FC = () => {
+    const {vmData} = useProxyData();
+    const {chatGPTOnly} = useChatGPTOnlyData();
+    const {mode} = useProxyMode();
+
+    const onChatGPTOnlyChange = useCallback(() => {
+        if (chrome.proxy === undefined || chrome.storage === undefined) return
+        chrome.storage.local.set({chatGPTOnly: !chatGPTOnly}) // toggle chatGPTOnly
+
+        if (vmData === null) return;
+        const socks5Profile = vmData._profiles.find((p) => p.type === "socks5");
+
+        // reconnect
+        chrome.runtime.sendMessage({type: "Connect", data: socks5Profile});
+    }, [chatGPTOnly, vmData])
+
+    return (
+        <div className="section glow">
+            {mode !== "disable" &&
+                <div className="black-cover">
+                    <Row className="justify-content-center align-items-center h-100">
+                        <Col xs={'auto'} className="text-center">
+                            <i className="bi bi-slash-circle fs-1"></i>
+                            <p>請先停用自訂代理規則</p>
+                        </Col>
+                    </Row>
+                </div>
+            }
+            <Row className="justify-content-center align-content-center">
+                <Col xs={'auto'}>
+                    <h5>ChatGPT Only Mode</h5>
+                </Col>
+                <div className="w-100"></div>
+                <Col xs={'auto'}>
+                    <small className="text-muted text-center"><p>開啟後只代理chatgpt.com和openai.com的網站</p></small>
+                </Col>
+                <Col xs={'auto'}>
+                    <FormCheck type="switch" id="chatGPTOnly">
+                        <FormCheckInput type="checkbox" checked={chatGPTOnly && mode === "disable"}
+                                        onChange={onChatGPTOnlyChange} style={{width: "4rem", height: "2rem"}}/>
+                    </FormCheck>
+                </Col>
+            </Row>
+        </div>
+    )
+}
---
+++ src/hooks/useProxyMode.ts
@@ -0,0 +1,47 @@
+import {useEffect, useState} from "react";
+
+/**
+ * Type definition for the proxy mode.
+ *
+ * @typedef {'whitelist' | 'blacklist' | 'disable'} ProxyMode
+ */
+type ProxyMode = 'whitelist' | 'blacklist' | 'disable';
+
+/**
+ * Custom hook to manage the proxy mode state.
+ *
+ * This hook retrieves the proxy mode from `chrome.storage.local` and listens for changes to update the state.
+ *
+ * @returns {Object} An object containing the current proxy mode.
+ */
+export default function useProxyMode() {
+    const [mode, setMode] = useState<ProxyMode>('disable');
+
+    useEffect(() => {
+        // Check if chrome.storage is available
+        if (chrome.storage === undefined) return;
+
+        // Get the proxyMode from chrome.storage.local
+        chrome.storage.local.get('proxyMode', (data) => {
+            console.debug(data) //debug
+            setMode(data.proxyMode ?? 'disable')
+        });
+
+        // Add listener for changes in chatGPTOnly
+        const listener = (changes: { [p: string]: chrome.storage.StorageChange }): void => {
+            console.debug(changes) //debug
+            if (changes.proxyMode) {
+                setMode(changes.proxyMode.newValue)
+            }
+        }
+        chrome.storage.onChanged.addListener(listener);
+
+        return () => {
+            chrome.storage.onChanged.removeListener(listener);
+        }
+    }, []);
+
+    return {mode}
+}
+
+export type {ProxyMode};
