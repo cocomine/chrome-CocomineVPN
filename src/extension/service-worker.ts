117094c34bd545fc5f17be1dbc45e486b5492539
cocomine <roguish.6888467468@gmail.com>
2026-04-10 20:42:33 +0800

    dev (#48)
    * Update .gitignore to include additional Node.js and JetBrains files
    
    * Update onChatGPTOnlyChange to use async storage operations for better performance
    
    * Refactor DynamicText component and move type definition to types.ts
    
    * Refactor DynamicText and LinkStatus components for improved readability and accessibility
    
    * Refactor prop documentation in DynamicText, LinkStatus, and TimeLast components for improved clarity
    
    * Refactor type definitions and improve documentation in types.ts and useProxyData.ts for clarity and consistency
    
    * Enhance documentation and improve functionality in ChatGPTOnly, DynamicText, and LinkStatus components
    
    * Update VPN extension to track connection and disconnection events; increment version to 2.1.0
    
    * Refactor VPN tracking logic to improve data handling; introduce enqueue mechanism for track data updates
    
    * Enhance error handling for track data updates and improve comments for clarity
    
    * ready releases
    
    * Add background permission and enhance window event logging for browser startup and closure
    
    * Implement heartbeat alarm and prevent simultaneous track data retrieval
    
    * Refactor track data handling and improve code formatting for consistency
    
    * Enhance messaging and version update for Cocomine VPN Extension
    
    - Updated version to 2.2.0 in manifest and package files
    - Added support for 'ConnectByExtension' message handling
    - Refactored message types for improved clarity and functionality
    - Revised README to reflect new features and changes
    
    * Fix service worker response handling to ensure proper message flow
    
    * Refactor message handling and improve code consistency in content and service worker scripts
    
    * Update README and types for improved message clarity and structure
    
    * Update connection protocol to HTTPS and enhance message handling for improved security and functionality
    
    * Refactor type definitions and improve boolean handling for connection status
    
    * Update message data structure and bump version to 2.3.2
    
    * Add null check for https_setting before saving to session storage
    
    * Bump version to 2.3.3 and update @types/chrome to 0.1.40
    
    * Refactor service worker code for consistency and readability
    
    * Change storage from session to local for HTTPS settings in service worker

--- src/extension/service-worker.ts
+++ src/extension/service-worker.ts
@@ -1,10 +1,10 @@
-import type {RuntimeMessage, StoredHTTPSCertData, StoredVmData, VMInstanceDataType} from './types';
+import type {RuntimeMessage, StoredHTTPSCertData, StoredVmData, VMInstanceDataType} from "./types";
 import {createProxyConfig} from "./createProxyConfig";
 import {logConnectTrack, logDisconnectTrack} from "./shared";
 
-const API_URL = 'https://api.cocomine.cc'; // API endpoint, in this time is only for ping test
-const PROXY_HOSTNAME = 'vpn.cocomine.cc'; // Proxy hostname
-const WEB_URL = process.env.NODE_ENV === "development" ? 'http://localhost:3000' : 'https://vpn.cocomine.cc'; // Web URL for user interactions
+const API_URL = "https://api.cocomine.cc"; // API endpoint, in this time is only for ping test
+const PROXY_HOSTNAME = "vpn.cocomine.cc"; // Proxy hostname
+const WEB_URL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://vpn.cocomine.cc"; // Web URL for user interactions
 let pingInterval: NodeJS.Timeout | undefined; // To hold the interval ID for pinging
 
 /**
@@ -13,13 +13,16 @@ let pingInterval: NodeJS.Timeout | undefined; // To hold the interval ID for pin
  * Depending on the reason for the event, it opens the README.md file on GitHub.
  */
 chrome.runtime.onInstalled.addListener(async ({reason}) => {
-    if (reason === 'install') {
+    if (reason === "install") {
         console.log("Extension installed");
-        await chrome.tabs.create({url: 'https://github.com/cocomine/chrome-vpn/blob/master/README.md'});
-    } else if (reason === 'update') {
+        await chrome.tabs.create({url: "https://github.com/cocomine/chrome-vpn/blob/master/README.md"});
+    } else if (reason === "update") {
         console.log("Extension updated");
-        await chrome.tabs.create({url: 'https://github.com/cocomine/chrome-vpn/blob/master/README.md#230'});
+        await chrome.tabs.create({url: "https://github.com/cocomine/chrome-vpn/blob/master/README.md#230"});
     }
+    await chrome.storage.local.setAccessLevel({
+        accessLevel: "TRUSTED_CONTEXTS"
+    });
 });
 
 /**
@@ -30,9 +33,9 @@ chrome.runtime.onInstalled.addListener(async ({reason}) => {
 chrome.alarms.onAlarm.addListener(async (alarm) => {
 
     // Check if the alarm is for offline time check
-    if (alarm.name === 'offline-time-check') {
+    if (alarm.name === "offline-time-check") {
 
-        const data = await chrome.storage.local.get<StoredVmData>('vmData');
+        const data = await chrome.storage.local.get<StoredVmData>("vmData");
         const vm = data.vmData;
         if (!vm) return; // No VM data found
         const expiresAt = new Date(vm._expired ?? Date.now()).getTime();
@@ -40,13 +43,13 @@ chrome.alarms.onAlarm.addListener(async (alarm) => {
         // Less than 1 hour left but more than 30 minutes left
         if (expiresAt - Date.now() <= 60 * 60 * 1000 && expiresAt - Date.now() > 30 * 60 * 1000) {
             console.log("VPN will close in less than 1 hour");
-            await chrome.notifications.clear('offlineTimeNotify');
-            await chrome.notifications.create('offlineTimeNotify', {
-                type: 'basic',
-                iconUrl: 'icon-128.png',
-                title: 'VPN節點即將關閉',
+            await chrome.notifications.clear("offlineTimeNotify");
+            await chrome.notifications.create("offlineTimeNotify", {
+                type: "basic",
+                iconUrl: "icon-128.png",
+                title: "VPN節點即將關閉",
                 message: `${vm._country}(${vm._name}) VPN節點即將1小時內關閉。如有需要請點擊延長開放時間。`,
-                buttons: [{title: '延長開放時間'}],
+                buttons: [{title: "延長開放時間"}],
             });
             return;
         }
@@ -54,56 +57,56 @@ chrome.alarms.onAlarm.addListener(async (alarm) => {
         // Less than 30 minutes left but more than 0 minutes left
         if (expiresAt - Date.now() <= 30 * 60 * 1000 && expiresAt - Date.now() > 0) {
             console.log("VPN will close in less than 30 minutes");
-            await chrome.notifications.clear('offlineTimeNotify');
-            await chrome.notifications.create('offlineTimeNotify', {
-                type: 'basic',
-                iconUrl: 'icon-128.png',
-                title: 'VPN節點即將關閉',
+            await chrome.notifications.clear("offlineTimeNotify");
+            await chrome.notifications.create("offlineTimeNotify", {
+                type: "basic",
+                iconUrl: "icon-128.png",
+                title: "VPN節點即將關閉",
                 message: `${vm._country}(${vm._name}) VPN節點即將30分鐘內關閉。如有需要請點擊延長開放時間。`,
-                buttons: [{title: '延長開放時間'}],
+                buttons: [{title: "延長開放時間"}],
             });
             return;
         }
 
         // Clear the current alarm
-        if (expiresAt - Date.now() <= 0) await chrome.alarms.clear('offline-time-check');
+        if (expiresAt - Date.now() <= 0) await chrome.alarms.clear("offline-time-check");
     }
 
     // Check if the alarm is for offline time reached
-    if (alarm.name === 'offline-time-reached') {
+    if (alarm.name === "offline-time-reached") {
         console.log("VPN time reached, disconnecting...");
 
         const vm = await disconnect();
         if (!vm) return;
-        await chrome.notifications.create('timeReachedNotify', {
-            type: 'basic',
-            iconUrl: 'icon-128.png',
-            title: 'VPN節點已關閉',
+        await chrome.notifications.create("timeReachedNotify", {
+            type: "basic",
+            iconUrl: "icon-128.png",
+            title: "VPN節點已關閉",
             message: `${vm._country}(${vm._name}) VPN節點已關閉。如有需要請重新連接。`,
-            buttons: [{title: '重新連接'}],
+            buttons: [{title: "重新連接"}],
         });
     }
 
     // Check if the alarm is for heartbeat
-    if (alarm.name === 'heartbeat') {
+    if (alarm.name === "heartbeat") {
         console.log("Heartbeat alarm triggered");
 
-        const data = await chrome.storage.local.get<StoredVmData>('vmData');
+        const data = await chrome.storage.local.get<StoredVmData>("vmData");
         const vm = data.vmData;
         if (!vm) return; // No VM data found
 
         // Send a heartbeat ping to the API
         try {
             const response = await fetch(`${API_URL}/ping`, {
-                method: 'HEAD',
+                method: "HEAD",
                 signal: AbortSignal.timeout(5000)
             });
-            if (!response.ok) throw new Error('Heartbeat ping failed'); // If response is not OK, throw an error
+            if (!response.ok) throw new Error("Heartbeat ping failed"); // If response is not OK, throw an error
 
-            console.log('Heartbeat ping successful');
+            console.log("Heartbeat ping successful");
             await logConnectTrack(vm); // Log the heartbeat as a connect event
         } catch (e) {
-            console.error('Heartbeat ping failed', e);
+            console.error("Heartbeat ping failed", e);
         }
     }
 });
@@ -115,8 +118,8 @@ chrome.alarms.onAlarm.addListener(async (alarm) => {
  */
 chrome.notifications.onClicked.addListener(async (notificationId) => {
     // Handle click event for 'offlineTimeNotify' notification
-    if (notificationId === 'offlineTimeNotify') {
-        const data = await chrome.storage.local.get<StoredVmData>('vmData');
+    if (notificationId === "offlineTimeNotify") {
+        const data = await chrome.storage.local.get<StoredVmData>("vmData");
         const vm = data.vmData;
         if (!vm) return; // No VM data found
 
@@ -125,7 +128,7 @@ chrome.notifications.onClicked.addListener(async (notificationId) => {
     }
 
     // Handle click event for 'timeReachedNotify' notification
-    if (notificationId === 'timeReachedNotify') {
+    if (notificationId === "timeReachedNotify") {
         await chrome.tabs.create({url: `${WEB_URL}`});
     }
 });
@@ -137,8 +140,8 @@ chrome.notifications.onClicked.addListener(async (notificationId) => {
  */
 chrome.notifications.onButtonClicked.addListener(async (notificationId, buttonIndex) => {
     // Handle button click event for 'offlineTimeNotify' notification
-    if (notificationId === 'offlineTimeNotify' && buttonIndex === 0) {
-        const data = await chrome.storage.local.get<StoredVmData>('vmData');
+    if (notificationId === "offlineTimeNotify" && buttonIndex === 0) {
+        const data = await chrome.storage.local.get<StoredVmData>("vmData");
         const vm = data.vmData;
         if (!vm) return; // No VM data found
 
@@ -147,7 +150,7 @@ chrome.notifications.onButtonClicked.addListener(async (notificationId, buttonIn
     }
 
     // Handle button click event for 'timeReachedNotify' notification
-    if (notificationId === 'timeReachedNotify' && buttonIndex === 0) {
+    if (notificationId === "timeReachedNotify" && buttonIndex === 0) {
         await chrome.tabs.create({url: `${WEB_URL}`});
     }
 });
@@ -158,19 +161,19 @@ chrome.notifications.onButtonClicked.addListener(async (notificationId, buttonIn
  * Specifically, it handles the 'Connect' message type to set up a proxy configuration.
  */
 chrome.runtime.onMessage.addListener((message: RuntimeMessage, _sender, sendResponse) => {
-    console.debug('Service worker received message:', message); // Debug log
+    console.debug("Service worker received message:", message); // Debug log
 
     // Handle the 'Connect' message type
-    if (message.type === 'Connect') {
+    if (message.type === "Connect") {
         (async () => {
             const {config, vmData, cleanup} = await createProxyConfig(message.data.vm_data);
             console.debug(config, vmData, cleanup); //debug
-            await chrome.proxy.settings.set({value: config, scope: 'regular'});
+            await chrome.proxy.settings.set({value: config, scope: "regular"});
             if (cleanup) cleanup();
 
             // save https setting to session storage for background use
-            if(message.data.setting){
-                await chrome.storage.session.set({'https_setting': message.data.setting});
+            if (message.data.setting) {
+                await chrome.storage.local.set({"https_setting": message.data.setting});
             }
 
             // every 1s, send request to /ping to check if the proxy is connected
@@ -180,34 +183,34 @@ chrome.runtime.onMessage.addListener((message: RuntimeMessage, _sender, sendResp
             pingInterval = setInterval(async () => {
                 try {
                     const response = await fetch(`${API_URL}/ping`, {
-                        method: 'HEAD',
+                        method: "HEAD",
                         signal: AbortSignal.timeout(1000)
                     });
-                    if (!response.ok) throw new Error('Ping failed'); // If response is not OK, throw an error to trigger the catch block
+                    if (!response.ok) throw new Error("Ping failed"); // If response is not OK, throw an error to trigger the catch block
 
                     // Ping successful
                     clearInterval(pingInterval);
                     await chrome.storage.local.set({vmData}); // Save the vmData to chrome.storage.local
 
                     // Notify the user that the connection is successful
-                    await chrome.notifications.clear('connectedNotify');
-                    await chrome.notifications.create('connectedNotify', {
-                        type: 'basic',
-                        iconUrl: 'icon-128.png',
-                        title: '成功連接',
-                        message: '已成功連接到節點!',
+                    await chrome.notifications.clear("connectedNotify");
+                    await chrome.notifications.create("connectedNotify", {
+                        type: "basic",
+                        iconUrl: "icon-128.png",
+                        title: "成功連接",
+                        message: "已成功連接到節點!",
                     });
 
                     await createAlarms(vmData); // Create alarms based on the VM data
                     sendResponse({connected: true}); // Send success response
 
-                    console.log('Ping successful, proxy is connected');
+                    console.log("Ping successful, proxy is connected");
 
                     // save connected time to track VPN usage
                     await logConnectTrack(vmData);
                 } catch (e) {
                     // Ping failed, increment tryCount and check if we should stop trying
-                    console.log('Ping attempt failed, retrying...', e);
+                    console.log("Ping attempt failed, retrying...", e);
                     tryCount++;
                     if (tryCount > 60) {
                         clearInterval(pingInterval);
@@ -221,13 +224,13 @@ chrome.runtime.onMessage.addListener((message: RuntimeMessage, _sender, sendResp
     }
 
     // Handle the 'Disconnect' message type
-    if (message.type === 'Disconnect') {
+    if (message.type === "Disconnect") {
         disconnect().then(() => sendResponse({connected: false})); // Send success response
         return true;
     }
 
     // Handle the 'AlarmsUpdate' message type
-    if (message.type === 'AlarmsUpdate') {
+    if (message.type === "AlarmsUpdate") {
         (async () => {
             const vmData = message.data;
 
@@ -239,7 +242,7 @@ chrome.runtime.onMessage.addListener((message: RuntimeMessage, _sender, sendResp
     }
 
     // Handle the 'ConnectByExtension' message type
-    if (message.type === 'ConnectByExtension') {
+    if (message.type === "ConnectByExtension") {
         (async () => {
             // Get the current proxy settings
             const config = await chrome.proxy.settings.get({});
@@ -247,7 +250,7 @@ chrome.runtime.onMessage.addListener((message: RuntimeMessage, _sender, sendResp
             console.debug(config); //debug
 
             // Check if connected to vpn.cocomine.cc
-            if (config.levelOfControl === 'controlled_by_this_extension' && (value.mode === 'fixed_servers' || value.mode === 'pac_script')) {
+            if (config.levelOfControl === "controlled_by_this_extension" && (value.mode === "fixed_servers" || value.mode === "pac_script")) {
                 sendResponse({connectByExtension: true});
             } else {
                 sendResponse({connectByExtension: false});
@@ -272,10 +275,10 @@ chrome.webRequest.onAuthRequired.addListener(function (details, callbackFn) {
         }
 
         // get credentials from session storage
-        chrome.storage.session.get<StoredHTTPSCertData>(['https_setting']).then((data) => {
+        chrome.storage.local.get<StoredHTTPSCertData>(["https_setting"]).then((data) => {
             // No HTTPS setting found
             if (!data.https_setting) {
-                console.error('No HTTPS setting found in session storage');
+                console.error("No HTTPS setting found in session storage");
                 callbackFn({cancel: true});
                 return;
             }
@@ -288,12 +291,12 @@ chrome.webRequest.onAuthRequired.addListener(function (details, callbackFn) {
                 }
             });
         }).catch((error) => {
-            console.error('Failed to retrieve HTTPS setting from session storage', error);
+            console.error("Failed to retrieve HTTPS setting from session storage", error);
             callbackFn({cancel: true});
         });
     },
-    {urls: ['<all_urls>']},
-    ['asyncBlocking']
+    {urls: ["<all_urls>"]},
+    ["asyncBlocking"]
 );
 
 /**
@@ -305,17 +308,17 @@ async function createAlarms(vmData: VMInstanceDataType) {
     // starting 1 hour before expiration
     await chrome.alarms.clearAll();
     const expiresAt = new Date(vmData._expired ?? Date.now()).getTime();
-    await chrome.alarms.create('offline-time-check', {
+    await chrome.alarms.create("offline-time-check", {
         periodInMinutes: 15,
         when: expiresAt - 60 * 60 * 1000
     });
     // Set up an alarm to disconnect when expiration time is reached
     // Note: This alarm will be created at the exact expiration time
-    await chrome.alarms.create('offline-time-reached', {
+    await chrome.alarms.create("offline-time-reached", {
         when: expiresAt
     });
     // Set up a heartbeat alarm every 60 minutes
-    await chrome.alarms.create('heartbeat', {
+    await chrome.alarms.create("heartbeat", {
         periodInMinutes: 60,
         delayInMinutes: 30
     });
@@ -326,23 +329,23 @@ async function createAlarms(vmData: VMInstanceDataType) {
  */
 async function disconnect(): Promise<VMInstanceDataType | undefined> {
     // Retrieve stored VM data
-    const stored = await chrome.storage.local.get<StoredVmData>('vmData');
+    const stored = await chrome.storage.local.get<StoredVmData>("vmData");
     const vmData = stored.vmData;
 
     // Clear proxy settings and stored VM data
-    clearInterval(pingInterval)
+    clearInterval(pingInterval);
     await chrome.proxy.settings.clear({});
-    await chrome.storage.local.remove('vmData');
-    await chrome.notifications.clear('disconnectedNotify');
-    await chrome.notifications.create('disconnectedNotify', {
-        type: 'basic',
-        iconUrl: 'icon-128.png',
-        title: '已斷開連接',
-        message: '已成功斷開與節點的連接!',
+    await chrome.storage.local.remove("vmData");
+    await chrome.notifications.clear("disconnectedNotify");
+    await chrome.notifications.create("disconnectedNotify", {
+        type: "basic",
+        iconUrl: "icon-128.png",
+        title: "已斷開連接",
+        message: "已成功斷開與節點的連接!",
     });
     await chrome.alarms.clearAll();
 
-    console.log('Disconnected from proxy');
+    console.log("Disconnected from proxy");
 
     // save disconnected time to track VPN usage
     if (vmData) {
